051b0b897bd563151b92df7914c84046bbd731a1
catetrai <caterina.trainito@gmail.com>
2016-09-04 12:24:17 +0200

    Modified edan_fftpower (added error message to ensure that input data is column vector)

--- edan/edan_fftpower.m
+++ edan/edan_fftpower.m
@@ -1,6 +1,12 @@
 function [faxis, Sxx] = edan_fftpower(x, Fs, T)
-% Computes power spectrum of x in decibel scale.
+% Computes power spectrum of vector x in decibel scale.
 % Fs: sampling frequency. T: total recording time in seconds.
+
+if length(x) == numel(x)    % if x is a vector
+    x = x(:);    % turn into column vector
+else
+    error('X must be a vector')
+end
 
 dt = 1/Fs;       % period in seconds
 df = 1/T;        % frequency resolution
@@ -8,12 +14,13 @@ fNQ = Fs/2;      % nyquist frequency
 
 % optional preprocessing steps
 x = hann(length(x)) .* x;     % apply Hanning taper
+
 % dfBin = 0.5;                  % change frequency binning
 % faxis = 0 : dfBin : fNQ;
 faxis = 0 : df : fNQ;
 
 xf = fft(x);
-Sxx = 2*dt^2/T * xf.*conj(xf);
+Sxx = abs(2*dt^2/T * xf.*conj(xf)); % take only magnitude
 Sxx = Sxx(1 : length(x)/2+1);
 Sxx = Sxx(1 : end/length(faxis) : end);
 Sxx = pow2db(Sxx);          % convert power to decibel scale
